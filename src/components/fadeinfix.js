e08be4478795c8cc7f0fe0799de11173dbb80b58
Timalexanderandersson <timalexanderandersson@hotmail.com>
2025-03-30 13:10:18 +0200

    change fadeinfix.js

--- src/components/fadeinfix.js
+++ src/components/fadeinfix.js
@@ -1,31 +1,25 @@
-
 import { useState, useRef, useEffect } from "react";
 import styles from '../styles/myprojects.module.css';
 
-
-/**
- * IntersectionObserverComponent is a React component that uses the Intersection Observer API
- * to detect when its child elements come into view. It applies a CSS class to the child elements
- * when they are visible in the viewport.
- *
- */
 const IntersectionObserverComponent = ({ children, options }) => {
     const [isView, setIsView] = useState(false);
     const targetRef = useRef(null);
 
     useEffect(() => {
+        const targetElement = targetRef.current; // Spara initialt ref-värde
+
+        if (!targetElement) return;
+
         const observer = new IntersectionObserver((entries) => {
             const [entry] = entries;
             setIsView(entry.isIntersecting);
         }, options);
 
-        if (targetRef.current) {
-            observer.observe(targetRef.current);
-        }
+        observer.observe(targetElement);
 
         return () => {
-            if (targetRef.current) {
-                observer.unobserve(targetRef.current);
+            if (targetElement) {
+                observer.unobserve(targetElement);
             }
         };
     }, [options]);
